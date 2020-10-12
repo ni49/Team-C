584d6c316910dfbb5f5c4134d1ffbe2c4818e64f
ScottishNobody <neilsonj273@gmail.com>
2020-10-12 12:42:34 +0100

    Very Basic Movement
    The most basic form of movement(having the player move purely across the screen with no inputs or stopping) is now implemented

--- John/script.js
+++ John/script.js
@@ -1,5 +1,8 @@
+var myPlayer;
+
 function startGame() {
     myGameArea.start();
+	myPlayer = new component(30, 30, "blue", 10, 120);
 }
 
 var myGameArea = {
@@ -9,5 +12,27 @@ var myGameArea = {
         this.canvas.height = window.innerHeight;
         this.context = this.canvas.getContext("2d");
         document.body.insertBefore(this.canvas, document.body.childNodes[0]);
-    }
+		this.interval = setInterval(updateGameArea, 20);
+    },
+	clear : function() {
+		this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+	}
+}
+
+function component(width, height, color, x, y) {
+    this.width = width;
+    this.height = height;
+    this.x = x;
+    this.y = y;  
+	this.update = function() {
+		ctx = myGameArea.context;
+		ctx.fillStyle = color;
+		ctx.fillRect(this.x, this.y, this.width, this.height);
+	}
+}
+
+function updateGameArea() {
+	myGameArea.clear();
+	myPlayer.x += 1;
+	myPlayer.update();
 }
