7bf229e1aae96bfaec064413f94498b14fa6a6fa
ni49 <71331353+ni49@users.noreply.github.com>
2020-10-27 21:13:19 +0000

    Added multi-connection

--- Nimer/app.js
+++ Nimer/app.js
@@ -6,6 +6,42 @@ app.get('/',function(req, res){
 	res.sendFile(__dirname + '/client/index.html');
 });
 
-app.use('/client' ,express.static(__dirname + '/client'));
+app.use('/client',express.static(__dirname + '/client'));
 
 serv.listen(2000);
+console.log("Server running");
+
+var SOCKET_LIST = {};
+
+var io = require('socket.io')(serv,{});
+io.sockets.on('connection', function(socket){
+	socket.id = Math.random();
+	socket.x = 0;
+	socket.y = 0;
+	socket.number = "" + Math.floor(10 * Math.random());
+	SOCKET_LIST[socket.id] = socket;
+	
+	socket.on('disconnect',function(){
+		delete SOCKET_LIST[socket.id];
+	});
+});
+
+
+setInterval(function(){
+	var pack = [];
+	for(var i in SOCKET_LIST){
+		var socket = SOCKET_LIST[i];
+		socket.x++;
+		socket.y++;
+		pack.push({
+			x:socket.x,
+			y:socket.y,
+			number:socket.number
+		});
+	
+	}
+	 for(var i in SOCKET_LIST){
+		var socket = SOCKET_LIST[i];
+		socket.emit('newPosition',pack);
+	}
+},1000/20);
