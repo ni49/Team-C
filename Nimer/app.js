32d335315723f30191cbc229b4552694e19e3b06
ni49 <71331353+ni49@users.noreply.github.com>
2020-12-04 16:21:36 +0000

    Add files via upload

--- Nimer/app.js
+++ Nimer/app.js
@@ -1,248 +1,57 @@
-var mongojs = require("mongojs");
-var db = mongojs('localhost:27017/myGame', ['account','progress']);
- 
-var express = require('express');
-var app = express();
-var serv = require('http').Server(app);
- 
-app.get('/',function(req, res) {
-	res.sendFile(__dirname + '/client/index.html');
+//Use required packages
+const Bundler = require('parcel-bundler');
+const app = require('express')();
+const server = require('http').createServer(app);
+const socketIO = require('socket.io');
+let io = socketIO(server);
+const file = 'index.html'
+const options = {}
+const bundler = new Bundler(file, options)
+app.use(bundler.middleware())
+//Port to launch localhost on
+const port = 2000
+//Lists used to store the data for the clients to read
+var nibble = {x: 50, y: 5}
+var players = []
+//World data
+const nibbleTotal = 2000
+const worldSize = 3500
+//Start server
+server.listen(port, () => {
+    console.log('Server is up on port ' + port);
+    //Create the world when the server starts
+    createWorld()
 });
-app.use('/client',express.static(__dirname + '/client'));
- 
-serv.listen(2000);
-console.log("Server started.");
- 
-var SOCKET_LIST = {};
- 
-var Entity = function(){
-	var self = {
-		x:250,
-		y:250,
-		spdX:0,
-		spdY:0,
-		id:"",
-	}
-	self.update = function(){
-		self.updatePosition();
-	}
-	self.updatePosition = function(){
-		self.x += self.spdX;
-		self.y += self.spdY;
-	}
-	self.getDistance = function(pt){
-		return Math.sqrt(Math.pow(self.x-pt.x,2) + Math.pow(self.y-pt.y,2));
-	}
-	return self;
+//When a player connects
+io.on('connection', (socket) => {
+    //On disconnect
+    socket.on('disconnect', () => {
+        //Update for players
+        socket.broadcast.emit('removal', socket.pos);
+    });
+    //Send the level data to the players
+    socket.emit('levelData', nibble)
+    //Recieve player data
+    socket.on('playerData', (dat) =>{
+        //Take player data, update values in players array and send back to other players
+        if(players.length > dat.id){
+            players[dat.id].x = dat.x
+            players[dat.id].y = dat.y
+            players[dat.id].radius = dat.radius
+            socket.broadcast.emit('playerData', {x: dat.x, y: dat.y, radius: dat.radius, id: dat.id})
+        } else {
+            players.push({x: dat.x, y: dat.y, radius: dat.radius, id: dat.id})
+            socket.broadcast.emit('playerData', {x: dat.x, y: dat.y, radius: dat.radius, id: dat.id})
+        }
+    })
+})
+//Create the world
+function createWorld(){
+    for(var i = 0; i < nibbleTotal; i++){
+        nibble[i] = {x: random(-worldSize, worldSize), y: random(-worldSize, worldSize)}
+    }
 }
- 
-var Player = function(id){
-	var self = Entity();
-	self.id = id;
-	self.number = "" + Math.floor(10 * Math.random());
-	self.pressingRight = false;
-	self.pressingLeft = false;
-	self.pressingUp = false;
-	self.pressingDown = false;
-	self.pressingAttack = false;
-	self.mouseAngle = 0;
-	self.maxSpd = 10;
- 
-	var super_update = self.update;
-	self.update = function(){
-		self.updateSpd();
-		super_update();
- 
-		if(self.pressingAttack){
-			self.shootBullet(self.mouseAngle);
-		}
-	}
-	self.shootBullet = function(angle){
-		var b = Bullet(self.id,angle);
-		b.x = self.x;
-		b.y = self.y;
-	}
- 
- 
-	self.updateSpd = function(){
-		if(self.pressingRight)
-			self.spdX = self.maxSpd;
-		else if(self.pressingLeft)
-			self.spdX = -self.maxSpd;
-		else
-			self.spdX = 0;
- 
-		if(self.pressingUp)
-			self.spdY = -self.maxSpd;
-		else if(self.pressingDown)
-			self.spdY = self.maxSpd;
-		else
-			self.spdY = 0;		
-	}
-	Player.list[id] = self;
-	return self;
+//Function to generate random number
+function random(min, max) {
+    return Math.floor(Math.random() * (max - min) ) + min;
 }
-Player.list = {};
-Player.onConnect = function(socket){
-	var player = Player(socket.id);
-	socket.on('keyPress',function(data){
-		if(data.inputId === 'left')
-			player.pressingLeft = data.state;
-		else if(data.inputId === 'right')
-			player.pressingRight = data.state;
-		else if(data.inputId === 'up')
-			player.pressingUp = data.state;
-		else if(data.inputId === 'down')
-			player.pressingDown = data.state;
-		else if(data.inputId === 'attack')
-			player.pressingAttack = data.state;
-		else if(data.inputId === 'mouseAngle')
-			player.mouseAngle = data.state;
-	});
-}
-Player.onDisconnect = function(socket){
-	delete Player.list[socket.id];
-}
-Player.update = function(){
-	var pack = [];
-	for(var i in Player.list){
-		var player = Player.list[i];
-		player.update();
-		pack.push({
-			x:player.x,
-			y:player.y,
-			number:player.number
-		});		
-	}
-	return pack;
-}
- 
- 
-var Bullet = function(parent,angle){
-	var self = Entity();
-	self.id = Math.random();
-	self.spdX = Math.cos(angle/180*Math.PI) * 10;
-	self.spdY = Math.sin(angle/180*Math.PI) * 10;
-	self.parent = parent;
-	self.timer = 0;
-	self.toRemove = false;
-	var super_update = self.update;
-	self.update = function(){
-		if(self.timer++ > 100)
-			self.toRemove = true;
-		super_update();
- 
-		for(var i in Player.list){
-			var p = Player.list[i];
-			if(self.getDistance(p) < 32 && self.parent !== p.id){
-				//handle collision. ex: hp--;
-				self.toRemove = true;
-			}
-		}
-	}
-	Bullet.list[self.id] = self;
-	return self;
-}
-Bullet.list = {};
- 
-Bullet.update = function(){
-	var pack = [];
-	for(var i in Bullet.list){
-		var bullet = Bullet.list[i];
-		bullet.update();
-		if(bullet.toRemove)
-			delete Bullet.list[i];
-		else
-			pack.push({
-				x:bullet.x,
-				y:bullet.y,
-			});		
-	}
-	return pack;
-}
- 
-var DEBUG = true;
- 
-var isValidPassword = function(data,cb){
-	db.account.find({username:data.username,password:data.password},function(err,res){
-		if(res.length > 0)
-			cb(true);
-		else
-			cb(false);
-	});
-}
-var isUsernameTaken = function(data,cb){
-	db.account.find({username:data.username},function(err,res){
-		if(res.length > 0)
-			cb(true);
-		else
-			cb(false);
-	});
-}
-var addUser = function(data,cb){
-	db.account.insert({username:data.username,password:data.password},function(err){
-		cb();
-	});
-}
- 
-var io = require('socket.io')(serv,{});
-io.sockets.on('connection', function(socket){
-	socket.id = Math.random();
-	SOCKET_LIST[socket.id] = socket;
- 
-	socket.on('signIn',function(data){
-		isValidPassword(data,function(res){
-			if(res){
-				Player.onConnect(socket);
-				socket.emit('signInResponse',{success:true});
-			} else {
-				socket.emit('signInResponse',{success:false});			
-			}
-		});
-	});
-	socket.on('signUp',function(data){
-		isUsernameTaken(data,function(res){
-			if(res){
-				socket.emit('signUpResponse',{success:false});		
-			} else {
-				addUser(data,function(){
-					socket.emit('signUpResponse',{success:true});					
-				});
-			}
-		});		
-	});
- 
- 
-	socket.on('disconnect',function(){
-		delete SOCKET_LIST[socket.id];
-		Player.onDisconnect(socket);
-	});
-	socket.on('sendMsgToServer',function(data){
-		var playerName = ("" + socket.id).slice(2,7);
-		for(var i in SOCKET_LIST){
-			SOCKET_LIST[i].emit('addToChat',playerName + ': ' + data);
-		}
-	});
- 
-	socket.on('evalServer',function(data){
-		if(!DEBUG)
-			return;
-		var res = eval(data);
-		socket.emit('evalAnswer',res);		
-	});
- 
- 
- 
-});
- 
-setInterval(function(){
-	var pack = {
-		player:Player.update(),
-		bullet:Bullet.update(),
-	}
- 
-	for(var i in SOCKET_LIST){
-		var socket = SOCKET_LIST[i];
-		socket.emit('newPositions',pack);
-	}
-},1000/25);
